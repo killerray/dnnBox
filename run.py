907e39162e233588b6f03613f75d188359082746
killerray <1130832130@qq.com>
2021-08-08 12:48:22 +0800

    feature:引入optuna,选择超参数

--- run.py
+++ run.py
@@ -9,12 +9,13 @@ from torchvision import datasets
 from torchvision.transforms import transforms
 from models import LeNet
 from train_torchmetrics import train_loop, test_loop
-# from train_ignite import test_loop, train_loop
+import optuna
+from optuna.trial import TrialState
 
 # 定义超参数
 batch_size = 64
 learning_rate = 1e-3
-num_epoches = 500
+num_epoches = 5
 
 train_dataset = datasets.MNIST(
     root='./mnist', train=True, transform=transforms.ToTensor(), download=True)
@@ -27,23 +28,45 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
-def LeNet_mnist_train():
+def LeNet_mnist_train(trial):
+    test_accuracy = None
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = LeNet(num_classes=10).to(device)
-
     loss_fn = nn.CrossEntropyLoss()
-    # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    optimizer = trial.suggest_categorical('optimizer',
+                                          [torch.optim.SGD,
+                                           torch.optim.RMSprop,
+                                           torch.optim.Adam])(
+        model.parameters(), lr=trial.suggest_loguniform('lr', 1e-3, 1e-2))
 
     for t in range(num_epoches):
         print(f"Epoch {t + 1}\n-------------------------------")
         train_loop(train_loader, model, loss_fn, optimizer)
-        test_loop(test_loader, model, loss_fn)
+        test_accuracy = test_loop(test_loader, model, loss_fn)
         print("Done!")
         # Save Models
     torch.save(model.state_dict(), "LeNet.pth")
     print("Saved PyTorch Model State to LeNet.pth")
+    return test_accuracy
 
 
 if __name__ == "__main__":
-    LeNet_mnist_train()
+    sampler = optuna.samplers.TPESampler()
+    study = optuna.create_study(sampler=sampler, direction='maximize')
+    study.optimize(func=LeNet_mnist_train, n_trials=20)
+    pruned_trials = study.get_trials(deepcopy=False, states=tuple([TrialState.PRUNED]))
+    complete_trials = study.get_trials(deepcopy=False, states=tuple([TrialState.COMPLETE]))
+
+    print("Study statistics: ")
+    print("  Number of finished trials: ", len(study.trials))
+    print("  Number of pruned trials: ", len(pruned_trials))
+    print("  Number of complete trials: ", len(complete_trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: ", trial.value)
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
--- train_torchmetrics.py
+++ train_torchmetrics.py
@@ -25,9 +25,6 @@ def train_loop(dataloader, model, loss_fn, optimizer):
 def test_loop(dataloader, model, loss_fn):
     # 实例化相关metrics的计算对象
     test_acc = torchmetrics.Accuracy()
-    test_recall = torchmetrics.Recall(average='none', num_classes=10)
-    test_precision = torchmetrics.Precision(average='none', num_classes=10)
-    test_auc = torchmetrics.AUROC(average="macro", num_classes=10)
 
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -40,27 +37,12 @@ def test_loop(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
             # 一个batch进行计算迭代
             test_acc(pred.argmax(1), y)
-            test_auc.update(pred, y)
-            test_recall(pred.argmax(1), y)
-            test_precision(pred.argmax(1), y)
 
     test_loss /= num_batches
     correct /= size
 
-    # 计算一个epoch的accuray、recall、precision、AUC
+    # 计算一个epoch的accuray
     total_acc = test_acc.compute()
-    total_recall = test_recall.compute()
-    total_precision = test_precision.compute()
-    total_auc = test_auc.compute()
-    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, "
-          f"Avg loss: {test_loss:>8f}, "
-          f"torch metrics acc: {(100 * total_acc):>0.1f}%\n")
-    print("recall of every test dataset class: ", total_recall)
-    print("precision of every test dataset class: ", total_precision)
-    print("auc:", total_auc.item())
-
     # 清空计算对象
-    test_precision.reset()
     test_acc.reset()
-    test_recall.reset()
-    test_auc.reset()
+    return total_acc
